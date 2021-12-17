85a7134c37a8995dffba69dfb80120cd49a82ae4
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-17 07:56:02 -0900

    add base classes with fields and test files

---
+++ VendorAndOrderTracker/Models/Order.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace PierresBakery.Models
+{
+  public class Order
+  {
+    public string Title {get; set;}
+    public string Description {get; set;}
+    public int Price {get; set;}
+    public string Date {get; set;}
+  }
+}
