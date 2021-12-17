07d0f4329193ba63d16f3071cb0e956bba96b513
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-17 10:16:58 -0900

    green test for GetAll(), add ClearAll()

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -6,8 +6,12 @@ using System;
 namespace PierresBakery.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
     [TestMethod]
     public void OrderConstructor_CreateInstanceOfOrder_Order()
     {
