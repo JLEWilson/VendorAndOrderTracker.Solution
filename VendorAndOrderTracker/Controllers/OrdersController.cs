afe31e69f2bad5728ab02e081903b1edda1cb449
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-17 14:52:17 -0900

    fixed missing bracket in Show route decorator that prevented website from loading

--- VendorAndOrderTracker/Controllers/OrdersController.cs
+++ VendorAndOrderTracker/Controllers/OrdersController.cs
@@ -6,13 +6,21 @@ namespace PierresBakery.Controllers
 {
   public class OrdersController : Controller
   {
-
     [HttpGet("/vendors/{vendorId}/orders/new")]
     public ActionResult New(int vendorId)
     {
-       Vendor vendor = Vendor.Find(vendorId);
-       return View(vendor);
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
     }
-
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
   }
 }
